e212eb8a00a9ec135d3e11e446b055e03351d1a0
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2021-03-23 23:21:01 -0300

    Files setted up

---
+++ main.py
@@ -0,0 +1,12 @@
+from PPlay.window import *
+
+window = Window(1000,600)
+keyboard = window.get_keyboard()
+
+assets = 'assets/'
+icons = 'assets/icons/'
+player = 'assets/player/'
+
+
+while(True):
+    window.update()
