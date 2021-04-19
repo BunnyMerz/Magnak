127deac3f73c6261c80799545fe57d2997b534ff
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2021-04-19 13:06:06 -0300

    Resumo para guiar nas proximas atualizações

--- tile.py
+++ tile.py
@@ -1,7 +1,8 @@
 from PPlay.sprite import *
 
 class Tile(Sprite):
-    def __init__(self, x=0, y=0, solid=False):
+    def __init__(self, x=0, y=0, z=0, solid=False):
         Sprite.__init__(self,'assets/tiles/ground.png',3)
         self.x = x
         self.y = y
+        self.z = z
